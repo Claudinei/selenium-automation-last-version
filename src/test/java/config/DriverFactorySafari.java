1c116863cbcb654dd7f08d12d8feff0b811c61e7
Claudinei <claudinei.bonilha@live.com>
2023-11-03 18:19:52 -0300

    Safari driver

---
+++ src/test/java/config/DriverFactorySafari.java
@@ -0,0 +1,38 @@
+package config;
+
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class DriverFactorySafari {
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if(driver == null) {
+            createDriver();
+        }
+        return driver;
+    }
+
+    private static void createDriver() {
+        WebDriverManager.getInstance(SafariDriver.class).setup();
+        driver = WebDriverManager.safaridriver().create();
+        //driver = new SafariDriver();
+        //SafariOptions safariOptions = new SafariOptions();
+        //driver = new SafariDriver(safariOptions);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    public static void killDriver() {
+        if(driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
