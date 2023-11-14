e639a760bf5be528aca5a63074857215659b77a0
Claudinei <claudinei.bonilha@live.com>
2023-11-14 16:50:26 -0300

    Ajustes no properties

--- src/test/java/config/Manipulador.java
+++ src/test/java/config/Manipulador.java
@@ -7,18 +7,17 @@ public class Manipulador {
 
     public static String  navegador;
 
-    public static Properties getProp() throws IOException {
+    public static Properties getProp(String propertiesPath) throws IOException {
         Properties props = new Properties();
-        FileInputStream file = new FileInputStream("properties/config.properties");
+        FileInputStream file = new FileInputStream(propertiesPath);
         props.load(file);
         return props;
     }
 
-    public static void  main(String args[]) throws IOException {
-
+    /*public static void  main(String args[]) throws IOException {
         Properties prop = getProp();
         navegador = prop.getProperty("prop.server.navegador");
 
         System.out.println("navegador = " + navegador);
-    }
+    }*/
 }
